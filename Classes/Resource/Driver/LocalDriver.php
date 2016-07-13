164931452812f6772d3e28031143cf2b4ae50231
Marcus Schwemer (in2code) <marcus.schwemer@in2code.de>
2016-07-13 09:52:17 +0200

    Update version number

--- Classes/Resource/Driver/LocalDriver.php
+++ Classes/Resource/Driver/LocalDriver.php
@@ -2,8 +2,6 @@
 namespace In2code\FalRecycler\Resource\Driver;
 
 /*
- * This file is part of the TYPO3 CMS project.
- *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
