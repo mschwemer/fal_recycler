48ddfe776bdaea7f03cab5168f52ad12c52b6873
Marcus Schwemer (in2code) <marcus.schwemer@in2code.de>
2016-07-13 09:47:10 +0200

    Add licensing information

--- Classes/Resource/Driver/LocalDriver.php
+++ Classes/Resource/Driver/LocalDriver.php
@@ -1,6 +1,19 @@
 <?php
 namespace In2code\FalRecycler\Resource\Driver;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use TYPO3\CMS\Core\Resource\FolderInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
